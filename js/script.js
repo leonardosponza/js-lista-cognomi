bf766cd48381a8b9f1d56db389fcb0351c11cc86
leonardo <sponzal4@gmail.com>
2021-05-31 15:22:57 +0200

    js

---
+++ js/script.js
@@ -0,0 +1,11 @@
+var utente = prompt('inserisci la tua email');
+
+var utenti = ['leo@gmail.com', 'pollo@gmail.com', 'nano@gmail.com', 'giu@gmail.com'];
+
+for(var i = 0; i < utenti.length; i++) 
+{
+    if(utente == utenti[i]){
+        found = 'Bentornato utente ' + utente;
+    }
+    else('errore mail non vailda')
+}
